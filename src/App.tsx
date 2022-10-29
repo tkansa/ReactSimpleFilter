b8bb21bfeb34e6ec8ad823fec3b5950bc1690b39
Tiia Kansa <como2e2@gmail.com>
2022-10-29 10:25:08 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fruit = ['apple', 'banana', 'orange', 'grapefruit',
+    'mango', 'strawberry', 'peach', 'apricot'];
+
+  const [filter, setFilter] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Type to filter the list:
+        <input id="filter"
+          name="filter"
+          type="text"
+          value={filter}
+          onChange={event => setFilter(event.target.value)}
+        />
+      </p>
+      <ul>
+        <ul>
+          {fruit.filter(f => f.includes(filter) || filter === '')
+            .map(f => <li key={f}>{f}</li>)}
+        </ul>
+      </ul>
     </div>
   );
 }
